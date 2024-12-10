a69d6a3c401ee7b5c3f4c961210afa2401a70c51
Grant <grfood2@yahoo.com>
2024-12-09 19:07:29 -0500

    Comments and cleanup

--- StarterCode/ContactTracer.java
+++ StarterCode/ContactTracer.java
@@ -16,8 +16,6 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
-import javax.sound.midi.SysexMessage;
-
 public class ContactTracer {
     public static final String DEFAULT_NAME = "IO/simple0.input";
     public static boolean[] visited;
@@ -54,16 +52,14 @@ public class ContactTracer {
 
             for (int i = 0; i < n; i++) {
                 String id = sc.nextLine();
-                System.out.println("DEBUG: Node " + i + ": ID=" + id);
+                // System.out.println("DEBUG: Node " + i + ": ID=" + id);
 
-                // You will want to store this ID. 
-                // Using a Hashmap, I would map ID to i, call it the id number.
+                // Stores ID and index in a hash table
                 table.put(id,i);
             }
-            System.out.println(table.toString());
 
-            // You will probably want to create an undirected graph G with n nodes
-            // Initially with no edges but add a method to add an edge between two nodes
+            // Undirected graph with n nodes 
+            // For each node, stores a list of neighboring nodes
             List<List<Integer>> graph = new ArrayList<>(n);
             for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
 
@@ -75,16 +71,14 @@ public class ContactTracer {
                 String idA = line[0];
                 String idB = line[1];
 
-                System.out.println("DEBUG: Contact between " + idA + " and " + idB);
-                // You might want to get the id number of idA and idB from the Hashmap
-                // Then add the edge between idA and idB to the graph
+                // System.out.println("DEBUG: Contact between " + idA + " and " + idB);
+
+                // Gets id of idA and idB from hash table and adds edge to the graph 
                 graph.get(table.get(idA)).add(table.get(idB));
                 graph.get(table.get(idB)).add(table.get(idA));
             }
-            System.out.println(graph.toString());
 
-            // Get how many contacts have been infected and how far to report exposure from
-            // an infected individual
+            // Get how many contacts have been infected and how far to report exposure from an infected individual
             line = sc.nextLine().split(" ");
             int numInfected = Integer.parseInt(line[0]);
             int distance = Integer.parseInt(line[1]);
@@ -92,24 +86,25 @@ public class ContactTracer {
             ArrayList<String> infected = new ArrayList<String>(numInfected);
             for (int c = 0; c < numInfected; c++) {
                 String idA = sc.nextLine();
-                System.out.println("DEBUG: Infected: " + idA);
+                // System.out.println("DEBUG: Infected: " + idA);
                 infected.add(idA);
             }
 
-            // Now process the information to get the results...
-            // Use the Graph, infected list, and distance to get the result and print the number of
-            // exposed individuals.
+            // Process results to calculate number of exposed individuals:
 
-            int exposedIncrement = 0;
+            int exposedIncrement = 0; // Starts the amount of people exposed at 0
 
+            // Sets all infected to visited by default
             for (int i = 0; i < numInfected; i++){
                 visited[table.get(infected.get(i))] = true;
             }
+
+            // Adds each new run of BFS to the current total of individuals exposed 
             for (int i = 0; i < numInfected; i++){
-                exposedIncrement += BFS(distance, table.get(infected.get(i)), graph, n, numInfected);
-                // System.out.println("Current exposed: " + BFS(distance, table.get(infected.get(i)), graph, n, numInfected));
+                exposedIncrement += BFS(distance, table.get(infected.get(i)), graph, n);
             }
 
+            // Final result (Adds exposed individuals and the infected individuals)
             System.out.println("TOTAL EXPOSED: " + (exposedIncrement));
 
         } catch (IOException e) {
@@ -117,27 +112,24 @@ public class ContactTracer {
         }
     }
 
-    // Breadth first search method
-    public static int BFS(int distance, int startIdNum, List<List<Integer>> graph, int n, int numInfected){
+    // BFS method (to determine all exposed neighbors of a certain node within a certain distance)
+    public static int BFS(int distance, int startIdNum, List<List<Integer>> graph, int n){
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{startIdNum, 0}); 
 
         int currentId = startIdNum; // Sets the current id to the starting id 
-        int numOfNeighbors = graph.get(currentId).size(); // Gets number of neighbors for each id
-        //System.out.println("Number of neighbors for node " + currentId + " = " + numOfNeighbors);
-        int exposed = 0; // Number of people exposed, starts with only those infected
+        int exposed = 0; // Number of people exposed, starts with 0 and adds originally infected on later
 
         visited[startIdNum] = true;
-        // queue.add(startIdNum);
 
-        // Iterate over the queue
+        // While the queue is not empty (there are still neighboring nodes to search)
         while(!queue.isEmpty()){
 
             int curr[] = queue.poll();
-            //System.out.print(curr + " ");
             currentId = curr[0];
             int currentDistance = curr[1];
 
+            // Increment exposed 
             exposed ++;
 
             // Stop exploring further if the distance is reached
@@ -149,23 +141,10 @@ public class ContactTracer {
             for (int neighbor : graph.get(currentId)) {
                 if (!visited[neighbor]) {
                     visited[neighbor] = true;
-                    queue.add(new int[]{neighbor, currentDistance + 1});
+                    queue.add(new int[]{neighbor, currentDistance + 1}); // Adds a neighbor to the queue only if it hasn't been visited
                 }
             }
         }
-
-       /*  for (int i = 0; i < numOfNeighbors; i++){ // For all of the current node's neighbors:
-            if(!visited[graph.get(currentId).get(i)]){
-                queue.add(graph.get(currentId).get(i)); // Add neighbor to the queue
-                exposed++; // Adds person to the list of exposed
-                System.out.println("Node " + graph.get(currentId).get(i) + " not visited, visiting. ");
-                System.out.println("Current exposed (for loop): " + exposed);
-            } else {
-                System.out.println("Node " + graph.get(currentId).get(i) + " already visited, skipping.");
-                System.out.println("Current exposed (for loop): " + exposed);
-            }
-            visited[graph.get(currentId).get(i)] = true;
-        } */
         
         return exposed;
     }
